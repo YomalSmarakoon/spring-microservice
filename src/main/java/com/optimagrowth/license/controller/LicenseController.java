4c0f1eeb0b2937a57308ef1443e0b4a91466c832
YomalSmarakoon <yomal.2018471@iit.ac.lk>
2025-07-29 14:57:34 +0530

    The developer's tale: license service-basic CRUD.
    (cherry picked from commit 6cd24303eebda923e70eb3e1cf3af239e85ccb56)

---
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -0,0 +1,60 @@
+package com.optimagrowth.license.controller;
+
+import com.optimagrowth.license.LicenseService;
+import com.optimagrowth.license.model.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/*
+ * Tells Spring Boot that this is a REST based service &
+ * it will automatically serialize/deserialize service requests/responses via JSON
+ */
+@RestController
+/*
+ * Expose all the HTTP endpoints in this class with a prefix of below
+ */
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicenseController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping(value = "/{licenceId}")
+    /*
+    * Also can use:
+    *       @RequestMapping(value = "/{licenceId}", method = RequestMethod.GET)
+    */
+    public ResponseEntity<License> getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenceId") String licenceId
+    ) {
+
+        License license = licenseService.getLicense(licenceId, organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request
+    ) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request
+    ) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @DeleteMapping(value = "/{licenceId}")
+    public ResponseEntity<String> deleteLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenceId") String licenceId
+    ) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenceId, organizationId));
+    }
+}
